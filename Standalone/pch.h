9fac4f1e0e15072cb57c062aa00ee8be7fd3a31b
Sven Schmidt <svenschmidt75@googlemail.com>
2013-03-03 07:31:07 -0600

    files added

---
+++ Standalone/pch.h
@@ -0,0 +1,10 @@
+/* all common and stable header files */
+
+#include <boost/filesystem.hpp>
+#include <boost/assert.hpp>
+
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <map>
