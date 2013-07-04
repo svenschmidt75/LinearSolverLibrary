93765d9333544df2874120a273348f999f2cee11
Sven Schmidt <svenschmidt75@googlemail.com>
2013-07-04 13:41:22 -0500

    LanczosPRO

---
+++ LinearSolverLibrary/lanczosPRO.cpp
@@ -0,0 +1,131 @@
+#include "pch.h"
+
+#include "lanczosPRO.h"
+#include "LinAlg/entities.h"
+#include "LinAlg/operators.h"
+#include "LinAlg/VectorMath.h"
+
+
+using namespace LinAlg_NS;
+
+
+namespace LinearSolverLibrary_NS {
+    
+namespace {
+
+    void printLanczosVectorsOrthogonal(std::vector<Vector> const & q, IMatrix2D::size_type size) {
+        /* The best we can hope for in orthogonality of the Lanczos vectors
+         * is <q_i, q_j> = eps if they are orthogonal.
+         * Here, we compute the exponent of how much the orthogonality deviates
+         * from eps.
+         * Example: <q_i, q_j> = 10^{-12}
+         * Result: 4, i.e. -12 + 16 (from eps) = 4
+         */
+        double const machine_eps = std::numeric_limits<double>::epsilon();
+        std::cout << std::endl;
+        for (IMatrix2D::size_type i = 0; i < size - 1; ++i) {
+            double angle = VectorMath::dotProduct(q[i], q[size - 1]);
+            double deviation = std::fabs(angle / machine_eps);
+            deviation = std::log10(deviation);
+            int int_deviation = static_cast<int>(boost::math::round(deviation));
+            std::cout << int_deviation << " ";
+        }
+    }
+}
+
+void
+LanczosPRO::init(LinAlg_NS::SparseMatrix2D const & A, Vector const & q0) const {
+    A_ = A;
+    IMatrix2D::size_type dim = A_.cols() * 10;
+    a.resize(dim);
+    b.resize(dim);
+    q.resize(dim, Vector(dim));
+    w1.resize(dim);
+    w2.resize(dim);
+    w3.resize(dim);
+
+    // 1st Lanczos vector
+    q[0] = q0;
+
+    // compute 2nd Lanczos vector
+    Vector w = A_ * q0;
+    double alpha = VectorMath::dotProduct(w, q0);
+    w -= alpha * q0;
+    double beta = VectorMath::norm(w);
+    q[1] = w * (1.0 / beta);
+
+    a[1] = alpha;
+    b[1] = beta;
+
+    current_lanczos_vector_index = 2;
+
+    monitorOrthogonality();
+}
+
+void
+LanczosPRO::computeNextLanczosVector() const {
+    //    BOOST_ASSERT_MSG(current_lanczos_vector_index < A_.cols(), "Lanczos::computeNextLanczosVector: Insufficient space");
+    Vector const & qn = q[current_lanczos_vector_index - 1];
+    double beta = getCurrentBeta();
+    Vector w = A_ * qn;
+    w -= beta * q[current_lanczos_vector_index - 2];
+    double alpha = VectorMath::dotProduct(w, qn);
+    w -= alpha * qn;
+    beta = VectorMath::norm(w);
+    q[current_lanczos_vector_index] = w * (1.0 / beta);
+
+    a[current_lanczos_vector_index] = alpha;
+    b[current_lanczos_vector_index] = beta;
+
+    current_lanczos_vector_index++;
+
+    monitorOrthogonality();
+}
+
+double
+LanczosPRO::getCurrentAlpha() const {
+    return a[current_lanczos_vector_index - 1];
+}
+
+double
+LanczosPRO::getCurrentBeta() const {
+    return b[current_lanczos_vector_index - 1];
+}
+
+Vector const &
+LanczosPRO::getPreviousLanczosVector() const {
+    return q[current_lanczos_vector_index - 2];
+}
+
+double
+LanczosPRO::getPreviousAlpha() const {
+    return a[current_lanczos_vector_index - 2];
+}
+
+double
+LanczosPRO::getPreviousBeta() const {
+    return b[current_lanczos_vector_index - 2];
+}
+
+void
+LanczosPRO::monitorOrthogonality() const {
+    double const eps = std::numeric_limits<double>::epsilon();
+    auto i = current_lanczos_vector_index - 2;
+    for (IMatrix2D::size_type j = 1; j <= i - 1; ++j) {
+        double beta_jp1 = b[j + 1];
+        double omega_i_jp1 = i == j + 1 ? 1.0 : w2[j + 1];
+        double aa = a[j] - a[i];
+        double omega_i_j = i == j ? 1.0 : w2[j];
+        double beta_j = b[j];
+        double omega_i_jm1 = i == j - 1 ? 1.0 : (j - 1 == 0 ? 0 : w2[j - 1]);
+        double beta_i = b[i];
+        double omega_j_im1 = j == i - 1 ? 1.0 : (i - 1 == 0 ? 0 : w1[j]);
+        double beta_ip1 = b[i + 1];
+        w3[j] = (beta_jp1 * omega_i_jp1 + aa * omega_i_j + beta_j * omega_i_jm1 - beta_i * omega_j_im1) / beta_ip1;
+    }
+    w3[i] = A_.cols() * 0.5 * std::sqrt(eps);
+
+    w2 = w3;
+}
+
+} // LinearSolverLibrary_NS
---
+++ LinearSolverLibrary/lanczosPRO.h
@@ -0,0 +1,62 @@
+/*
+ * Name  : LanczosPRO
+ * Path  : 
+ * Use   : Lanczos algorithm with partial reorthogonalization
+ * Author: Sven Schmidt
+ * Date  : 07/04/2013
+ */
+#pragma once
+
+#include "DeclSpec.h"
+
+#include "LinAlg/entities.h"
+
+
+#pragma warning(disable:4275)
+#pragma warning(disable:4251)
+
+namespace LinearSolverLibrary_NS {
+
+    class LINEARSOLVERLIBRARY_DECL_SYMBOLS LanczosPRO final : boost::noncopyable {
+    public:
+        void init(LinAlg_NS::SparseMatrix2D const & A, LinAlg_NS::Vector const & q0) const;
+
+        template<typename EXPR, typename BINOP>
+        void init(LinAlg_NS::SparseMatrix2D const & A, LinAlg_NS::internal::ScalarVectorBinaryExpr<EXPR, BINOP> const & q0) const {
+            // really need inheriting constructors here :-(
+            LinAlg_NS::Vector q;
+            q = q0;
+            init(A, q);
+        }
+
+        void computeNextLanczosVector() const;
+        double getCurrentAlpha() const;
+        double getCurrentBeta() const;
+        LinAlg_NS::Vector const & getPreviousLanczosVector() const;
+        double getPreviousAlpha() const;
+        double getPreviousBeta() const;
+
+    private:
+        void monitorOrthogonality() const;
+
+    private:
+        // orthogonalized via Lanczos
+        mutable std::vector<LinAlg_NS::Vector> q;
+        mutable LinAlg_NS::IMatrix2D::size_type current_lanczos_vector_index;
+
+        // alphas and bets in T_i
+        mutable std::vector<double> a;
+        mutable std::vector<double> b;
+
+        // for monitoring the orthogonality
+        mutable std::vector<double> w1;
+        mutable std::vector<double> w2;
+        mutable std::vector<double> w3;
+
+        mutable LinAlg_NS::SparseMatrix2D A_;
+    };
+
+} // LinearSolverLibrary_NS
+
+#pragma warning(default:4251)
+#pragma warning(default:4275)
