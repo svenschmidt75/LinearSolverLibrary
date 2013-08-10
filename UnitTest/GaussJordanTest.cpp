2a7e7afb825447c8105de2ea07f40f6071d2ec6e
Sven Schmidt <svenschmidt75@googlemail.com>
2013-08-10 11:08:11 -0500

    more Gauss-Jordan tests

--- UnitTest/GaussJordanTest.cpp
+++ UnitTest/GaussJordanTest.cpp
@@ -20,7 +20,7 @@ void
 GaussJordanTest::tearDown() {}
 
 void
-GaussJordanTest::Test2by2() {
+GaussJordanTest::Test2by2_1() {
     Matrix2D A(2, 2);
 
     A(0, 0) = 4;
@@ -37,7 +37,7 @@ GaussJordanTest::Test2by2() {
     rhs(1) = 1;
 
     Matrix2D AInverse(2, 2);
-    Vector x(4);
+    Vector x(2);
     bool success;
     std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
 
@@ -46,11 +46,183 @@ GaussJordanTest::Test2by2() {
     x_ref = AInverse * rhs;
 
     CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
-
 }
 
 void
-GaussJordanTest::Test4by4() {
+GaussJordanTest::Test2by2_2() {
+    Matrix2D A(2, 2);
+
+    A(0, 0) = 1E-20;
+    A(0, 1) = 1;
+
+    A(1, 0) = 1;
+    A(1, 1) = 1;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(2);
+    rhs(0) = 1;
+    rhs(1) = 1;
+
+    Matrix2D AInverse(2, 2);
+    Vector x(2);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(2);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
+
+void
+GaussJordanTest::Test3by3_1() {
+    Matrix2D A(3, 3);
+
+    A(0, 0) = 2;
+    A(0, 1) = -1;
+    A(0, 2) = 0;
+
+    A(1, 0) = -1;
+    A(1, 1) = 2;
+    A(1, 2) = -1;
+
+    A(2, 0) = 0;
+    A(2, 1) = -1;
+    A(2, 2) = 2;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(3);
+    rhs(0) = 1;
+    rhs(1) = 1;
+    rhs(2) = 1;
+
+    Matrix2D AInverse(3, 3);
+    Vector x(3);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(3);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
+
+void
+GaussJordanTest::Test3by3_2() {
+    Matrix2D A(3, 3);
+
+    A(0, 0) = 1;
+    A(0, 1) = 2;
+    A(0, 2) = 3;
+
+    A(1, 0) = 0;
+    A(1, 1) = 1;
+    A(1, 2) = 4;
+
+    A(2, 0) = 5;
+    A(2, 1) = 6;
+    A(2, 2) = 0;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(3);
+    rhs(0) = 1;
+    rhs(1) = 1;
+    rhs(2) = 1;
+
+    Matrix2D AInverse(3, 3);
+    Vector x(3);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(3);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
+
+void
+GaussJordanTest::Test3by3_3() {
+    Matrix2D A(3, 3);
+
+    A(0, 0) = 1;
+    A(0, 1) = 3;
+    A(0, 2) = 3;
+
+    A(1, 0) = 1;
+    A(1, 1) = 4;
+    A(1, 2) = 3;
+
+    A(2, 0) = 1;
+    A(2, 1) = 3;
+    A(2, 2) = 4;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(3);
+    rhs(0) = 1;
+    rhs(1) = 1;
+    rhs(2) = 1;
+
+    Matrix2D AInverse(3, 3);
+    Vector x(3);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(3);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
+
+void
+GaussJordanTest::Test3by3_4() {
+    Matrix2D A(3, 3);
+
+    A(0, 0) = 1;
+    A(0, 1) = 2;
+    A(0, 2) = 0;
+
+    A(1, 0) = 3;
+    A(1, 1) = 4;
+    A(1, 2) = 4;
+
+    A(2, 0) = 5;
+    A(2, 1) = 6;
+    A(2, 2) = 3;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(3);
+    rhs(0) = 3;
+    rhs(1) = 7;
+    rhs(2) = 8;
+
+    Matrix2D AInverse(3, 3);
+    Vector x(3);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(3);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
+
+void
+GaussJordanTest::Test4by4_1() {
     Matrix2D A(4, 4);
 
     A(0, 0) = 2;
@@ -92,5 +264,94 @@ GaussJordanTest::Test4by4() {
     x_ref = AInverse * rhs;
 
     CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
 
+void
+GaussJordanTest::Test4by4_2() {
+    Matrix2D A(4, 4);
+
+    A(0, 0) = 1;
+    A(0, 1) = -1;
+    A(0, 2) = 0;
+    A(0, 3) = 0;
+
+    A(1, 0) = -1;
+    A(1, 1) = 2;
+    A(1, 2) = -1;
+    A(1, 3) = 0;
+
+    A(2, 0) = 0;
+    A(2, 1) = -1;
+    A(2, 2) = 2;
+    A(2, 3) = -1;
+
+    A(3, 0) = 0;
+    A(3, 1) = 0;
+    A(3, 2) = -1;
+    A(3, 3) = 2;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(4);
+    rhs(0) = 1;
+    rhs(1) = 1;
+    rhs(2) = 1;
+    rhs(3) = 1;
+
+    Matrix2D AInverse(4, 4);
+    Vector x(4);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(4);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
+}
+
+void
+GaussJordanTest::Test4by4_3() {
+    Matrix2D A(4, 4);
+
+    A(0, 0) = 2;
+    A(0, 1) = 3;
+    A(0, 2) = 1;
+    A(0, 3) = 5;
+
+    A(1, 0) = 6;
+    A(1, 1) = 13;
+    A(1, 2) = 5;
+    A(1, 3) = 19;
+
+    A(2, 0) = 2;
+    A(2, 1) = 19;
+    A(2, 2) = 10;
+    A(2, 3) = 23;
+
+    A(3, 0) = 4;
+    A(3, 1) = 10;
+    A(3, 2) = 11;
+    A(3, 3) = 31;
+
+    A.print();
+
+    // generate r.h.s.
+    Vector rhs(4);
+    rhs(0) = 1;
+    rhs(1) = 1;
+    rhs(2) = 1;
+    rhs(3) = 1;
+
+    Matrix2D AInverse(4, 4);
+    Vector x(4);
+    bool success;
+    std::tie(success, AInverse, x) = LinearSolverLibrary_NS::GaussJordan().solve(A, rhs);
+
+
+    Vector x_ref(4);
+    x_ref = AInverse * rhs;
+
+    CPPUNIT_ASSERT_MESSAGE("mismatch in Gauss solver result", SparseLinearSolverUtil::isVectorEqual(x, x_ref, 1E-10));
 }
--- UnitTest/GaussJordanTest.h
+++ UnitTest/GaussJordanTest.h
@@ -14,8 +14,15 @@
 
 class GaussJordanTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(GaussJordanTest);
-//    CPPUNIT_TEST(Test2by2);
-    CPPUNIT_TEST(Test4by4);
+    CPPUNIT_TEST(Test2by2_1);
+    CPPUNIT_TEST(Test2by2_2);
+    CPPUNIT_TEST(Test3by3_1);
+    CPPUNIT_TEST(Test3by3_2);
+    CPPUNIT_TEST(Test3by3_3);
+    CPPUNIT_TEST(Test3by3_4);
+    CPPUNIT_TEST(Test4by4_1);
+    CPPUNIT_TEST(Test4by4_2);
+    CPPUNIT_TEST(Test4by4_3);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -23,6 +30,13 @@ public:
     void tearDown();
 
 protected:
-    void Test2by2();
-    void Test4by4();
+    void Test2by2_1();
+    void Test2by2_2();
+    void Test3by3_1();
+    void Test3by3_2();
+    void Test3by3_3();
+    void Test3by3_4();
+    void Test4by4_1();
+    void Test4by4_2();
+    void Test4by4_3();
 };
