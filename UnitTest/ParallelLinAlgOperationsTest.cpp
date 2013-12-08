060100b1b36e1e8cf08aff8214cc3efa087a51e2
Sven Schmidt <svenschmidt75@googlemail.com>
2013-12-08 12:33:47 -0600

    cleanup

--- UnitTest/ParallelLinAlgOperationsTest.cpp
+++ UnitTest/ParallelLinAlgOperationsTest.cpp
@@ -9,16 +9,25 @@
 #include <ppl.h>
 
 
-
-#include <atomic>
-
-
-
 using namespace EntityReader_NS;
 using namespace LinAlg_NS;
 using namespace LinearSolverLibrary_NS;
 
 namespace FS = boost::filesystem;
+
+
+// use this to work with a small matrix/vector
+#define SIMPLE 1
+#undef SIMPLE
+
+
+#ifdef SIMPLE
+    std::string const filename = R"(\Develop\SparseMatrixData\bcsstk05\bcsstk05.ar)";
+    IMatrix2D::size_type const dim = 153;
+#else
+    std::string const filename = R"(\Develop\SparseMatrixData\language\language.ar)";
+    IMatrix2D::size_type const dim = 399130;
+#endif
 
 
 void
@@ -31,9 +40,7 @@ namespace {
 
     SparseMatrix2D
     readMatrix() {
-//        FS::path filename("\\Develop\\SparseMatrixData\\bcsstk05\\bcsstk05.ar");
-        FS::path filename("\\Develop\\SparseMatrixData\\language\\language.ar");
-        ISparseMatrixReader::Ptr sm_reader = SparseMatrixReaderCreator::create(filename.string());
+        ISparseMatrixReader::Ptr sm_reader = SparseMatrixReaderCreator::create(filename);
         CPPUNIT_ASSERT_MESSAGE("File not found", sm_reader);
         CPPUNIT_ASSERT_MESSAGE("error reading sparse matrix data", sm_reader->read());
         SparseMatrix2D const m = sm_reader->get();
@@ -48,7 +55,7 @@ namespace {
     }
 
     Vector
-    SerialMatrixVectorMultiplication( SparseMatrix2D const & m, Vector const & x) {
+    serialMatrixVectorMultiplication( SparseMatrix2D const & m, Vector const & x) {
         Vector result{x.size()};
         for (IMatrix2D::size_type row = 0; row < x.size(); ++row) {
             double result_row = LinAlg_NS::helper::matrix_vector_mul<Vector>(m, x, row);
@@ -58,7 +65,7 @@ namespace {
     }
 
     Vector
-    ParallelMatrixVectorMultiplication(SparseMatrix2D const & m, Vector const & x) {
+    nonChunkedParallelMatrixVectorMultiplication(SparseMatrix2D const & m, Vector const & x) {
         Vector result{x.size()};
         concurrency::parallel_for(IMatrix2D::size_type{0}, x.size(), [&m, &x, &result](IMatrix2D::size_type row) {
             double result_row = LinAlg_NS::helper::matrix_vector_mul<Vector>(m, x, row);
@@ -66,56 +73,6 @@ namespace {
         }, concurrency::static_partitioner());
         return result;
     }
-
-    /*
-
-    Vector
-    ParallelMatrixVectorMultiplication2(SparseMatrix2D const & m, Vector const & x) {
-    int numberOfProcessors = std::thread::hardware_concurrency();
-    numberOfProcessors = 8;
-
-    int size = 23;
-
-    int size_per_chunk = size / numberOfProcessors;
-    int diff = size % numberOfProcessors;
-    if (diff > numberOfProcessors) {
-    size_per_chunk++;
-    diff -= numberOfProcessors;
-    }
-
-    for (int i = 0; i < size - diff; i += size_per_chunk) {
-
-    int start_index = i;
-    int end_size = i + size_per_chunk;
-    int chunk_index = i / size_per_chunk;
-    int i_copy = i;
-    if (diff) {
-    if (chunk_index < diff) {
-    i_copy += chunk_index;
-    end_size = i_copy + size_per_chunk + 1;
-    }
-    else {
-    i_copy += diff;
-    end_size = i_copy + size_per_chunk;
-    }
-    }
-
-    std::cout << "Chunk index: " << chunk_index << std::endl;
-    std::cout << "Start index: " << start_index << std::endl;
-    std::cout << "Adjusted start index: " << i_copy << std::endl;
-    std::cout << "End index: " << i + size_per_chunk << std::endl;
-    std::cout << "Adjusted end index: " << end_size << std::endl;
-
-    for (int j = i_copy; j < end_size; ++j) {
-    std::cout << "i, j: " << i_copy << ", " << j << std::endl;
-    }
-
-    }
-
-    return Vector{1};
-    }
-
-    */
 
     template<typename T>
     std::tuple<T, T>
@@ -149,32 +106,7 @@ namespace {
     }
 
     Vector
-    ParallelMatrixVectorMultiplication2(SparseMatrix2D const & m, Vector const & x) {
-        int numberOfProcessors = std::thread::hardware_concurrency();
-        numberOfProcessors = 3;
-        int size = 19;
-
-        int size_per_chunk = size / numberOfProcessors;
-        int diff = size % numberOfProcessors;
-
-        for (int i = 0; i < size - diff; i += size_per_chunk) {
-
-            int chunk_index = i / size_per_chunk;
-            int start_index, end_size;
-            std::tie(start_index, end_size) = getChunkStartEndIndex(size, numberOfProcessors, i);
-
-            std::cout << "Chunk index: " << chunk_index << std::endl;
-            for (int j = start_index; j < end_size; ++j) {
-                std::cout << "i, j: " << start_index << ", " << j << std::endl;
-            }
-
-        }
-
-        return Vector{1};
-    }
-
-    Vector
-    ParallelMatrixVectorMultiplication3(SparseMatrix2D const & m, Vector const & x) {
+    chunkedParallelMatrixVectorMultiplication(SparseMatrix2D const & m, Vector const & x) {
         using size_type = IMatrix2D::size_type;
         Vector result{x.size()};
 
@@ -199,7 +131,8 @@ namespace {
         HighResTimer() : start_(boost::chrono::high_resolution_clock::now()) {}
         ~HighResTimer() {
             auto end = boost::chrono::high_resolution_clock::now();
-            auto d = (end - start_).count() * ((double)boost::chrono::high_resolution_clock::period::num / boost::chrono::high_resolution_clock::period::den);
+            auto frac = (double)boost::chrono::high_resolution_clock::period::num / boost::chrono::high_resolution_clock::period::den;
+            auto d = (end - start_).count() * frac;
             std::cout << std::endl << "Duration: " << d << std::endl;
         }
 
@@ -211,48 +144,44 @@ namespace {
 
 void
 ParallelLinAlgOperationsTest::TestParallelMatrixVectorMultiplication() {
-//    IMatrix2D::size_type const dim = 153;
-    IMatrix2D::size_type const dim = 399130;
     Vector x = createVectorOfSize(dim);
     SparseMatrix2D const & m = readMatrix();
 
     Vector serial_result;
     {
         HighResTimer t;
-        serial_result = SerialMatrixVectorMultiplication(m, x);
+        serial_result = serialMatrixVectorMultiplication(m, x);
     }
 
     Vector parallel_result;
     {
         HighResTimer t;
-        parallel_result = ParallelMatrixVectorMultiplication(m, x);
+        parallel_result = nonChunkedParallelMatrixVectorMultiplication(m, x);
     }
     CPPUNIT_ASSERT_MESSAGE("mismatch in linear solver result", SparseLinearSolverUtil::isVectorEqual(serial_result, parallel_result, 1E-15));
 }
 
 void
 ParallelLinAlgOperationsTest::TestParallelChunkedMatrixVectorMultiplication() {
-//    IMatrix2D::size_type const dim = 153;
-    IMatrix2D::size_type const dim = 399130;
     Vector x = createVectorOfSize(dim);
     SparseMatrix2D const & m = readMatrix();
 
     Vector serial_result;
     {
         HighResTimer t;
-        serial_result = SerialMatrixVectorMultiplication(m, x);
+        serial_result = serialMatrixVectorMultiplication(m, x);
     }
 
     Vector parallel_result;
     {
         HighResTimer t;
-        parallel_result = ParallelMatrixVectorMultiplication(m, x);
+        parallel_result = nonChunkedParallelMatrixVectorMultiplication(m, x);
     }
     CPPUNIT_ASSERT_MESSAGE("mismatch in linear solver result", SparseLinearSolverUtil::isVectorEqual(serial_result, parallel_result, 1E-15));
 
     {
         HighResTimer t;
-        parallel_result = ParallelMatrixVectorMultiplication3(m, x);
+        parallel_result = chunkedParallelMatrixVectorMultiplication(m, x);
     }
     CPPUNIT_ASSERT_MESSAGE("mismatch in linear solver result", SparseLinearSolverUtil::isVectorEqual(serial_result, parallel_result, 1E-15));
 }
