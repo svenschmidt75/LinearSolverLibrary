64c110d38c3aba2e9ac8683d7f9cfc1f0d78cecd
Sven Schmidt <svenschmidt75@googlemail.com>
2013-12-24 05:54:41 -0600

    add files

---
+++ LinAlg/ConstColumnIteratorForwardDecl.h
@@ -0,0 +1,18 @@
+/*
+* Name  : ConstColumnIteratorForwardDecl
+* Path  :
+* Use   : forward declaration
+* Author: Sven Schmidt
+* Date  : 09/28/2013
+*/
+#pragma once
+
+
+namespace LinAlg_NS {
+
+
+template<typename T>
+class ConstColumnIterator;
+
+
+} // namespace LinAlg_NS
---
+++ LinAlg/ConstRowIteratorForwardDecl.h
@@ -0,0 +1,18 @@
+/*
+* Name  : ConstRowIteratorForwardDecl
+* Path  :
+* Use   : forward declaration
+* Author: Sven Schmidt
+* Date  : 11/29/2013
+*/
+#pragma once
+
+
+namespace LinAlg_NS {
+
+
+    template<typename T>
+    class ConstRowIterator;
+
+
+} // namespace LinAlg_NS
